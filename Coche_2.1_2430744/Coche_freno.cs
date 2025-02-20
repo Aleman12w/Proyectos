cc9d5afa1cb58e8aa172173918ff389483d90706
Aleman12w <pjonny909@gmail.com>
2025-02-20 06:18:33 -0600

    Add files via upload

---
+++ Coche_2.1_2430744/Coche_freno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coche_2._1_2430744
+{
+    public class Cochefreno : Coche
+    {
+        public Cochefreno(string marca, string modelo, string velocidad_maxima, string velocidad_actual, int velocidad)
+            : base(marca, modelo, velocidad_actual, velocidad_maxima, velocidad)
+        {
+
+        }
+
+        public void Frenar(int freno)
+        {
+            Console.WriteLine("El carro acelera " + freno);
+            this.Velocidad = Convert.ToInt32(freno = 0);
+        }
+
+    }
+}
---
+++ Coche_2.1_2430744/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coche_2._1_2430744
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche coche = new Coche("Chevrolet", "Aveo", "60km/h", "150km/h", 0);
+            Console.WriteLine("Descripcion del Coche:");
+            Console.WriteLine("Marca:" + coche.Marca);
+            Console.WriteLine("Modelo:" + coche.Modelo);
+            Console.WriteLine("Velocidad actual:" + coche.Velocidad_actual);
+            Console.WriteLine("Velocidad maxima:" + coche.Velocidad_maxima);
+
+            coche.acelera(10);
+            Console.WriteLine("El coche acelera:" + coche.Velocidad);
+            coche.decelerar(10);
+            Console.WriteLine("El coche decelera:" + coche.Velocidad);
+
+
+            Cochefreno cochefreno = new Cochefreno("Chevrolet", "Aveo", "60km/h", "150km/h", 0);
+            Console.WriteLine("Descripcion del Coche:");
+            Console.WriteLine("Marca:" + coche.Marca);
+            Console.WriteLine("Modelo:" + coche.Modelo);
+            Console.WriteLine("Velocidad actual:" + coche.Velocidad_actual);
+            Console.WriteLine("Velocidad maxima:" + coche.Velocidad_maxima);
+
+            coche.acelera(10);
+            Console.WriteLine("El coche acelera:" + coche.Velocidad);
+            coche.decelerar(10);
+            Console.WriteLine("El coche decelera:" + coche.Velocidad);
+            cochefreno.Frenar(0);
+            Console.WriteLine("El coche frena:" + coche.Velocidad);
+
+            Console.ReadKey();
+        }
+    }
+}
